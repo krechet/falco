3b6cb216e67386b9a4ece229ec085836bf5f6bb1
Andrey Vorobiev <tgr@inbox.ru>
2013-01-06 23:56:44 +0400

    next

--- public/scripts/views/loader.js
+++ public/scripts/views/loader.js
@@ -1,7 +1,7 @@
 function AjaxLoader(){
     
     this.$el = $('<div id="ajaxLoader"/>');
-    this.$el.append('<img src="/images/335.gif"/>');
+    this.$el.append('<img src="/images/335s.gif"/>');
     
     $('body').append(this.$el);
     
